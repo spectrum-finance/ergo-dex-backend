d241b25ff40917a61c4da699344c4bb114ee3f51
oskin1 <ilya.arcadich@gmail.com>
2020-05-06 21:33:42 +0300

    Context cleanup.

--- modules/dex-watcher/src/main/scala/org/ergoplatform/dex/watcher/context/WatcherContext.scala
+++ modules/dex-watcher/src/main/scala/org/ergoplatform/dex/watcher/context/WatcherContext.scala
@@ -3,12 +3,10 @@ package org.ergoplatform.dex.watcher.context
 import org.ergoplatform.dex.domain.Order
 import org.ergoplatform.dex.domain.models.Transaction
 import org.ergoplatform.dex.streaming.{Consumer, Producer}
-import tofu.optics.macros.{promote, ClassyOptics}
+import tofu.optics.macros.{ClassyOptics, promote}
 
 @ClassyOptics
 final case class WatcherContext[F[_]](
   @promote consumer: Consumer[F, Transaction],
-  @promote producer: Producer[F, Order[_]],
-  subscribeTopic: String,
-  publishTopic: String
+  @promote producer: Producer[F, Order[_]]
 )
