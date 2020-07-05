5f430a8d74bb72294b0af999b7c61b3b2b509700
oskin1 <ilya.arcadich@gmail.com>
2020-07-05 14:19:34 +0300

    TransactionService WIP.

--- modules/dex-executor/src/main/scala/org/ergoplatform/dex/executor/services/TransactionService.scala
+++ modules/dex-executor/src/main/scala/org/ergoplatform/dex/executor/services/TransactionService.scala
@@ -1,11 +1,36 @@
 package org.ergoplatform.dex.executor.services
 
-import org.ergoplatform.ErgoLikeTransaction
+import cats.Monad
+import cats.data.NonEmptyList
 import org.ergoplatform.dex.domain.models.Match.AnyMatch
+import org.ergoplatform.dex.domain.syntax.boxId._
+import org.ergoplatform.dex.domain.syntax.match_._
+import org.ergoplatform.{ErgoBoxCandidate, ErgoLikeTransaction, Input}
+import sigmastate.interpreter.ProverResult
+import tofu.syntax.monadic._
 
 abstract class TransactionService[F[_]] {
 
   /** Assembly Ergo transaction from a given `match`.
     */
   def makeTx(anyMatch: AnyMatch): F[ErgoLikeTransaction]
+}
+
+object TransactionService {
+
+  final private class Live[F[_]: Monad] extends TransactionService[F] {
+
+    def makeTx(anyMatch: AnyMatch): F[ErgoLikeTransaction] = {
+      val in  = inputs(anyMatch).toList.toVector
+      val out = outputs(anyMatch).toList.toVector
+      ErgoLikeTransaction(in, out).pure
+    }
+  }
+
+  private[services] def inputs(anyMatch: AnyMatch): NonEmptyList[Input] =
+    anyMatch.allOrders.map { ord =>
+      Input(ord.meta.boxId.toErgo, ProverResult.empty)
+    }
+
+  private[services] def outputs(anyMatch: AnyMatch): NonEmptyList[ErgoBoxCandidate] = ???
 }
