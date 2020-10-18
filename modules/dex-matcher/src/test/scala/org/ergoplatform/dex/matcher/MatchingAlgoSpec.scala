9646629f457d1378a833189da8401b1a947e4a8b
oskin1 <ilya.arcadich@gmail.com>
2020-10-18 08:29:37 +0300

    One to many match spec.

--- modules/dex-matcher/src/test/scala/org/ergoplatform/dex/matcher/MatchingAlgoSpec.scala
+++ modules/dex-matcher/src/test/scala/org/ergoplatform/dex/matcher/MatchingAlgoSpec.scala
@@ -9,7 +9,7 @@ import org.scalatestplus.scalacheck.ScalaCheckDrivenPropertyChecks
 
 class MatchingAlgoSpec extends PropSpec with Matchers with ScalaCheckDrivenPropertyChecks {
 
-  property("Simple match") {
+  property("1 to 1 match, full") {
     val gen =
       for {
         assetX      <- assetIdGen
@@ -23,6 +23,32 @@ class MatchingAlgoSpec extends PropSpec with Matchers with ScalaCheckDrivenPrope
     forAll(gen) { case (ask, bid) =>
       val matcher = MatchingAlgo.instance[Eval]
       val result  = matcher(List(ask), List(bid)).value
+      result.foreach { case Trade(order, counterOrders) =>
+        counterOrders.toList.foreach { co =>
+          co.quoteAsset shouldBe order.quoteAsset
+          co.baseAsset shouldBe order.baseAsset
+        }
+        order.amount shouldBe counterOrders.map(_.amount).toList.sum
+      }
+    }
+  }
+
+  property("1 to many match, full") {
+    val gen =
+      for {
+        assetX      <- assetIdGen
+        assetY      <- assetIdGen
+        askAmount   <- Gen.chooseNum(1000L, 10000000L)
+        bidAmount   <- Gen.chooseNum(100L, 900L)
+        price       <- Gen.chooseNum(2L, 1000L)
+        feePerToken <- Gen.chooseNum(100L, 1000L)
+        ask         <- askGen(assetX, assetY, askAmount, price, feePerToken)
+        bid0        <- bidGen(assetX, assetY, bidAmount, price, feePerToken)
+        bid1        <- bidGen(assetX, assetY, askAmount - bidAmount, price, feePerToken)
+      } yield (ask, bid0, bid1)
+    forAll(gen) { case (ask, bid0, bid1) =>
+      val matcher = MatchingAlgo.instance[Eval]
+      val result  = matcher(List(ask), List(bid0, bid1)).value
       result.foreach { case Trade(order, counterOrders) =>
         counterOrders.toList.foreach { co =>
           co.quoteAsset shouldBe order.quoteAsset
