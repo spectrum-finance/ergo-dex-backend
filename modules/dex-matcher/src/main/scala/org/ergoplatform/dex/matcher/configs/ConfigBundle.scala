14494673930039a87d4409242094fdfacbfad4db
oskin1 <ilya.arcadich@gmail.com>
2021-07-04 17:32:36 +0300

    Fmt.

--- modules/dex-matcher/src/main/scala/org/ergoplatform/dex/matcher/configs/ConfigBundle.scala
+++ modules/dex-matcher/src/main/scala/org/ergoplatform/dex/matcher/configs/ConfigBundle.scala
@@ -3,21 +3,21 @@ package org.ergoplatform.dex.matcher.configs
 import derevo.derive
 import derevo.pureconfig.pureconfigReader
 import org.ergoplatform.common.db.PgConfig
-import org.ergoplatform.dex.configs.{ConfigBundleCompanion, ConsumerConfig, KafkaConfig, ProducerConfig}
 import org.ergoplatform.common.streaming.CommitPolicy
+import org.ergoplatform.dex.configs.{ConfigBundleCompanion, ConsumerConfig, KafkaConfig, ProducerConfig}
 import tofu.Context
 import tofu.logging.Loggable
-import tofu.optics.macros.{ClassyOptics, promote}
+import tofu.optics.macros.{promote, ClassyOptics}
 
 @derive(pureconfigReader)
 @ClassyOptics
 final case class ConfigBundle(
-                               @promote matcher: MatcherConfig,
-                               @promote db: PgConfig,
-                               @promote commitPolicy: CommitPolicy,
-                               @promote consumer: ConsumerConfig,
-                               producer: ProducerConfig,
-                               @promote kafka: KafkaConfig
+  @promote matcher: MatcherConfig,
+  @promote db: PgConfig,
+  @promote commitPolicy: CommitPolicy,
+  @promote consumer: ConsumerConfig,
+  producer: ProducerConfig,
+  @promote kafka: KafkaConfig
 )
 
 object ConfigBundle extends Context.Companion[ConfigBundle] with ConfigBundleCompanion[ConfigBundle] {
