fd86cd9bf8807cf7f73e4205ecfc53f46edbc436
oskin1 <ilya.arcadich@gmail.com>
2021-12-09 15:45:57 +0300

    WIP: Sql updated.

--- modules/markets-api/src/main/scala/org/ergoplatform/dex/markets/db/sql/AnalyticsSql.scala
+++ modules/markets-api/src/main/scala/org/ergoplatform/dex/markets/db/sql/AnalyticsSql.scala
@@ -23,14 +23,14 @@ class AnalyticsSql(implicit lg: LogHandler) {
     sql"""
          |select distinct on (p.pool_id) p.pool_id, p.x_id, sx.tx, p.x_ticker, p.y_id, sx.ty, p.y_ticker from pools p
          |left join (
-         |	select
-         |		s.pool_id,
-         |		cast(sum(case when (s.input_id = p.y_id) then s.output_amount else 0 end) as BIGINT) as tx,
-         |		cast(sum(case when (s.input_id = p.x_id) then s.output_amount else 0 end) as BIGINT) as ty
-         |	from swaps s
-         |	left join pools p on p.pool_state_id = s.pool_state_id
-         |	where s.timestamp >= $fromTs
-         |	group by s.pool_id
+         |  select
+         |    s.pool_id,
+         |    cast(sum(case when (s.input_id = p.y_id) then s.output_amount else 0 end) as BIGINT) as tx,
+         |    cast(sum(case when (s.input_id = p.x_id) then s.output_amount else 0 end) as BIGINT) as ty
+         |  from swaps s
+         |  left join pools p on p.pool_state_id = s.pool_state_id
+         |  where s.timestamp >= $fromTs
+         |  group by s.pool_id
          |) as sx on sx.pool_id = p.pool_id
          |where sx.pool_id is not null
          """.query[PoolVolumeSnapshot]
