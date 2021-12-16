11d508e2399deff3072fd180bc6762a79f274b64
oskin1 <ilya.arcadich@gmail.com>
2021-12-16 13:57:20 +0300

    Docs.

--- modules/markets-api/src/main/scala/org/ergoplatform/dex/markets/repositories/Pools.scala
+++ modules/markets-api/src/main/scala/org/ergoplatform/dex/markets/repositories/Pools.scala
@@ -5,9 +5,15 @@ import org.ergoplatform.ergo.TokenId
 
 trait Pools[F[_]] {
 
+  /** Get snapshots of all pools.
+    */
   def snapshots: F[List[PoolSnapshot]]
 
+  /** Get snapshots of those pools that involve the given asset.
+    */
   def snapshotsByAsset(asset: TokenId): F[List[PoolSnapshot]]
 
+  /** Get recent volumes by all pools.
+    */
   def volumes(fromTs: Long): F[List[PoolVolumeSnapshot]]
 }
