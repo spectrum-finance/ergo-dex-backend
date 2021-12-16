740638926bb50de99e8ec17409fad2ae7ba46e80
oskin1 <ilya.arcadich@gmail.com>
2021-12-16 18:59:52 +0300

    Sql extended.

--- modules/markets-api/src/main/scala/org/ergoplatform/dex/markets/db/sql/AnalyticsSql.scala
+++ modules/markets-api/src/main/scala/org/ergoplatform/dex/markets/db/sql/AnalyticsSql.scala
@@ -2,8 +2,11 @@ package org.ergoplatform.dex.markets.db.sql
 
 import doobie.LogHandler
 import doobie.implicits._
+import doobie.util.fragment.Fragment
 import doobie.util.query.Query0
-import org.ergoplatform.dex.markets.db.models.{PoolSnapshot, PoolVolumeSnapshot}
+import org.ergoplatform.common.models.TimeWindow
+import org.ergoplatform.dex.domain.amm.PoolId
+import org.ergoplatform.dex.markets.db.models.{PoolFeesSnapshot, PoolSnapshot, PoolVolumeSnapshot}
 
 class AnalyticsSql(implicit lg: LogHandler) {
 
@@ -21,7 +24,12 @@ class AnalyticsSql(implicit lg: LogHandler) {
          |where px.gindex = p.gindex
          """.stripMargin.query[PoolSnapshot]
 
-  def getPoolVolumes(fromTs: Long): Query0[PoolVolumeSnapshot] =
+  def getPoolVolumes(tw: TimeWindow): Query0[PoolVolumeSnapshot] = {
+    val tsCond =
+      Fragment.const(
+        s"where ${tw.from.map(ts => s"s.timestamp >= $ts").getOrElse("")} ${if (tw.from.isDefined && tw.to.isDefined) "and"
+        else ""} ${tw.to.map(ts => s"s.timestamp <= $ts").getOrElse("")}"
+      )
     sql"""
          |select distinct on (p.pool_id)
          |  p.pool_id,
@@ -41,11 +49,79 @@ class AnalyticsSql(implicit lg: LogHandler) {
          |    cast(sum(case when (s.input_id = p.x_id) then s.output_amount else 0 end) as BIGINT) as ty
          |  from swaps s
          |  left join pools p on p.pool_state_id = s.pool_state_id
-         |  where s.timestamp >= $fromTs
+         |  $tsCond
          |  group by s.pool_id
          |) as sx on sx.pool_id = p.pool_id
          |left join assets ax on ax.id = p.x_id
          |left join assets ay on ay.id = p.y_id
          |where sx.pool_id is not null
          """.query[PoolVolumeSnapshot]
+  }
+
+  def getPoolFees(tw: TimeWindow): Query0[PoolFeesSnapshot] = {
+    val tsCond =
+      Fragment.const(
+        s"where ${tw.from.map(ts => s"s.timestamp >= $ts").getOrElse("")} ${if (tw.from.isDefined && tw.to.isDefined) "and"
+        else ""} ${tw.to.map(ts => s"s.timestamp <= $ts").getOrElse("")}"
+      )
+    sql"""
+         |select distinct on (p.pool_id)
+         |  p.pool_id,
+         |  p.x_id,
+         |  sx.tx,
+         |  ax.ticker,
+         |  ax.decimals,
+         |  p.y_id,
+         |  sx.ty,
+         |  ay.ticker,
+         |  ay.decimals
+         |from pools p
+         |left join (
+         |  select
+         |    s.pool_id,
+         |    cast(sum(case when (s.input_id = p.y_id) then s.output_amount::decimal * p.fee_num / 1000 else 0 end) as bigint) as tx,
+         |    cast(sum(case when (s.input_id = p.x_id) then s.output_amount::decimal * p.fee_num / 1000 else 0 end) as bigint) as ty
+         |  from swaps s
+         |  left join pools p on p.pool_state_id = s.pool_state_id
+         |  $tsCond
+         |  group by s.pool_id
+         |) as sx on sx.pool_id = p.pool_id
+         |left join assets ax on ax.id = p.x_id
+         |left join assets ay on ay.id = p.y_id
+         |where sx.pool_id is not null
+         """.query[PoolFeesSnapshot]
+  }
+
+  def getPoolFees(poolId: PoolId, tw: TimeWindow): Query0[PoolFeesSnapshot] = {
+    val tsCond =
+      Fragment.const(
+        s"${tw.from.map(ts => s"and s.timestamp >= $ts").getOrElse("")} ${tw.to.map(ts => s"and s.timestamp <= $ts").getOrElse("")}"
+      )
+    sql"""
+         |select distinct on (p.pool_id)
+         |  p.pool_id,
+         |  p.x_id,
+         |  sx.tx,
+         |  ax.ticker,
+         |  ax.decimals,
+         |  p.y_id,
+         |  sx.ty,
+         |  ay.ticker,
+         |  ay.decimals
+         |from pools p
+         |left join (
+         |  select
+         |    s.pool_id,
+         |    cast(sum(case when (s.input_id = p.y_id) then s.output_amount::decimal * p.fee_num / 1000 else 0 end) as bigint) as tx,
+         |    cast(sum(case when (s.input_id = p.x_id) then s.output_amount::decimal * p.fee_num / 1000 else 0 end) as bigint) as ty
+         |  from swaps s
+         |  left join pools p on p.pool_state_id = s.pool_state_id
+         |  where p.pool_id = $poolId $tsCond
+         |  group by s.pool_id
+         |) as sx on sx.pool_id = p.pool_id
+         |left join assets ax on ax.id = p.x_id
+         |left join assets ay on ay.id = p.y_id
+         |where sx.pool_id is not null
+         """.query[PoolFeesSnapshot]
+  }
 }
