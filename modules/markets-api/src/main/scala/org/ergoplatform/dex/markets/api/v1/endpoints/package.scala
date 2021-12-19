fb2f0176f0563b77af4b278dc42fb6352478260d
oskin1 <ilya.arcadich@gmail.com>
2021-12-19 18:03:36 +0300

    API description.

--- modules/markets-api/src/main/scala/org/ergoplatform/dex/markets/api/v1/endpoints/package.scala
+++ modules/markets-api/src/main/scala/org/ergoplatform/dex/markets/api/v1/endpoints/package.scala
@@ -17,24 +17,31 @@ import sttp.tapir.{
   Validator
 }
 import sttp.tapir.generic.auto._
-import sttp.tapir.json.circe._
 
 package object endpoints {
 
   implicit def schemaBigInt: Schema[BigInt] = Schema.schemaForBigDecimal.map(_.toBigIntExact)(BigDecimal(_))
 
+  val VersionPrefix = "v1"
+
   val baseEndpoint: Endpoint[Unit, HttpError, Unit, Any] =
-    endpoint.errorOut(
-      oneOf[HttpError](
-        oneOfMapping(StatusCode.NotFound, jsonBody[HttpError.NotFound].description("not found")),
-        oneOfMapping(StatusCode.NoContent, emptyOutputAs(HttpError.NoContent)),
-        oneOfDefaultMapping(jsonBody[HttpError.Unknown].description("unknown"))
+    endpoint
+      .in(VersionPrefix)
+      .errorOut(
+        oneOf[HttpError](
+          oneOfMapping(StatusCode.NotFound, jsonBody[HttpError.NotFound].description("not found")),
+          oneOfMapping(StatusCode.NoContent, emptyOutputAs(HttpError.NoContent)),
+          oneOfDefaultMapping(jsonBody[HttpError.Unknown].description("unknown"))
+        )
       )
-    )
 
   def timeWindow: EndpointInput[TimeWindow] =
-    (query[Option[Long]]("from").validateOption(Validator.min(0L)) and
-      query[Option[Long]]("to").validateOption(Validator.min(0L)))
+    (query[Option[Long]]("from")
+      .description("Window lower bound (UNIX timestamp millis)")
+      .validateOption(Validator.min(0L)) and
+      query[Option[Long]]("to")
+        .description("Window upper bound (UNIX timestamp millis)")
+        .validateOption(Validator.min(0L)))
       .map { input =>
         TimeWindow(input._1, input._2)
       } { case TimeWindow(from, to) => from -> to }
