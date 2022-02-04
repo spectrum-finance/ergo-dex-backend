40dfaf9dff7ea67b0b7d7a80bb5ce2af0ad22cd4
oskin1 <ilya.arcadich@gmail.com>
2022-02-04 12:30:35 +0300

    Rates conversion fixed.

--- modules/dex-core/src/main/scala/org/ergoplatform/dex/domain/Market.scala
+++ modules/dex-core/src/main/scala/org/ergoplatform/dex/domain/Market.scala
@@ -15,6 +15,15 @@ final case class Market(x: AssetClass, y: AssetClass, price: Price) {
 
 object Market {
 
+  val Base10 = BigDecimal(10)
+
   def fromReserves(rx: FullAsset, ry: FullAsset): Market =
-    Market(rx.assetClass, ry.assetClass, Price(BigDecimal(ry.amount) / rx.amount, BigDecimal(rx.amount) / ry.amount))
+    Market(
+      rx.assetClass,
+      ry.assetClass,
+      Price(
+        BigDecimal(ry.amount) / rx.amount * Base10.pow(rx.evalDecimals - ry.evalDecimals),
+        BigDecimal(rx.amount) / ry.amount * Base10.pow(ry.evalDecimals - rx.evalDecimals)
+      )
+    )
 }
