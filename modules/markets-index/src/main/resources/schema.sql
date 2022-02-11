83df083720d0d0d02ff926c1705cd6d8a1d38559
oskin1 <ilya.arcadich@gmail.com>
2022-02-11 15:17:35 +0300

    Minor fixes.

--- modules/markets-index/src/main/resources/schema.sql
+++ modules/markets-index/src/main/resources/schema.sql
@@ -117,4 +117,4 @@ create table if not exists public.lq_locks (
 
 alter table public.lq_locks owner to ergo_admin;
 
-create index lq_locks__asset_id on public.lq_locks using btree (asset_id);
+create index lq_locks__asset_id on public.lq_locks using btree (token_id);
