e80b31f79151e99ed0e31e53bbd0e1c656f75e48
t.gusev <gusev.t.s1@gmail.com>
2022-09-06 20:56:18 +0300

    fixed order executor fee parser

--- modules/utxo-tracker/src/main/scala/org/ergoplatform/dex/tracker/parsers/amm/OrderExecutorFeeParser.scala
+++ modules/utxo-tracker/src/main/scala/org/ergoplatform/dex/tracker/parsers/amm/OrderExecutorFeeParser.scala
@@ -30,17 +30,23 @@ object OrderExecutorFeeParser {
       ts: Long,
       pool: Option[CFMMPool]
     ): Option[OrderExecutorFee] = {
+      val rewardAddress = o match {
+        case swap: CFMMVersionedOrder.SwapV0       => swap.params.redeemer
+        case swap: CFMMVersionedOrder.SwapV1       => swap.params.redeemer
+        case deposit: CFMMVersionedOrder.DepositV0 => deposit.params.redeemer
+        case deposit: CFMMVersionedOrder.DepositV1 => deposit.params.redeemer
+        case redeem: CFMMVersionedOrder.RedeemV0   => redeem.params.redeemer
+        case redeem: CFMMVersionedOrder.RedeemV1   => redeem.params.redeemer
+      }
+
       val tree     = ErgoTreeSerializer.default.deserialize(output.ergoTree)
       val template = ErgoTreeTemplate.fromBytes(tree.template)
-      if (reservedErgoTrees.contains(template) || output.assets.nonEmpty) none
-      else {
-        val tree    = ErgoTreeSerializer.default.deserialize(output.ergoTree)
-        val address = e.fromProposition(tree).toOption
-        val fee     = output.value
-        address.map(a =>
-          OrderExecutorFee(pool.map(_.poolId), o.id, output.boxId, e.toString(a), fee, ts)
-        )
-      }
+      val address  = e.fromProposition(tree).toOption.map(e.toString)
+
+      if (reservedErgoTrees.contains(template) || output.assets.nonEmpty || address.contains(rewardAddress.unwrapped))
+        none
+      else
+        address.map(a => OrderExecutorFee(pool.map(_.poolId), o.id, output.boxId, a, output.value, ts))
     }
   }
 }
