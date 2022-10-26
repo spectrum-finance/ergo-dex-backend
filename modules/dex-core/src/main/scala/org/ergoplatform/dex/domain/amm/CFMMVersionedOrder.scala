b6264c44f0890150cd30cf4f8da2204e4863c1e6
t.gusev <gusev.t.s1@gmail.com>
2022-10-26 19:44:28 +0400

    try this decoder

--- modules/dex-core/src/main/scala/org/ergoplatform/dex/domain/amm/CFMMVersionedOrder.scala
+++ modules/dex-core/src/main/scala/org/ergoplatform/dex/domain/amm/CFMMVersionedOrder.scala
@@ -92,7 +92,7 @@ object CFMMVersionedOrder {
 
     implicit val decoderDepositV0: Decoder[DepositV0] =
       deriveDecoder
-        .validate(_.get("version").toOption.nonEmpty, "There is no version in deposit version v0.")
+        .validate(_.field("version").success.nonEmpty, "There is no version in deposit version v0.")
   }
 
   @derive(encoder, decoder, loggable)
