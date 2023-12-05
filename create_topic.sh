84cb66be73546d973ead7e2d2d2f34d43f66a649
t.gusev <gusev.t.s1@gmail.com>
2023-12-05 15:48:02 +0300

    dc fixed

--- create_topic.sh
+++ create_topic.sh
@@ -1,5 +1,5 @@
 #/bin/bash
 
-/opt/bitnami/kafka/bin/kafka-topics.sh --create --topic mempool-tx-topic --bootstrap-server 0.0.0.0:9092
-/opt/bitnami/kafka/bin/kafka-topics.sh --create --topic ledger-tx-topic --bootstrap-server 0.0.0.0:9092
-/opt/bitnami/kafka/bin/kafka-topics.sh --bootstrap-server 0.0.0.0:9092 --list
+/opt/bitnami/kafka/bin/kafka-topics.sh --create --topic mempool-tx-topic --bootstrap-server localhost:9092
+/opt/bitnami/kafka/bin/kafka-topics.sh --create --topic ledger-tx-topic --bootstrap-server localhost:9092
+/opt/bitnami/kafka/bin/kafka-topics.sh --bootstrap-server localhost:9092 --list
